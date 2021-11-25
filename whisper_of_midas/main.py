7c8dd40711930d3b2254fc7ba441f25b0469a862
michalkulisiewicz <michalkulisiewicz@gmail.com>
2021-11-25 20:19:22 +0100

    Added method that creates candlestick chart for visualisation of data

--- whisper_of_midas/main.py
+++ whisper_of_midas/main.py
@@ -23,6 +23,26 @@ def convert_unix_timestamp(df):
     df['Close.time'] = pd.to_datetime(df['Close.time'], unit='ms')
     return df
 
+def create_candlestick_chart():
+    # # Create subplots and mention plot grid size
+
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
+                        vertical_spacing=0.03, subplot_titles=('OHLC', 'Volume'),
+                        row_width=[0.2, 0.7])
+
+    # Plot OHLC on 1st row
+    fig.add_trace(go.Candlestick(x=df["Close.time"], open=df["Open"], high=df["High"],
+                                 low=df["Low"], close=df["Close"], name="OHLC"),
+                  row=1, col=1
+                  )
+
+    # Bar trace for volumes on 2nd row without legend
+    fig.add_trace(go.Bar(x=df['Close.time'], y=df['Volume'].astype(float).round(12), showlegend=False), row=2, col=1)
+
+    # Do not show OHLC's rangeslider plot
+    fig.update(layout_xaxis_rangeslider_visible=False)
+    return fig
+
 if __name__ == '__main__':
     credentials = Credentials()
     binance_credentials = credentials.get_auth_tokens()
