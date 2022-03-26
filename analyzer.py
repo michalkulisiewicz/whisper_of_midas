20324e397dd55241ba0b3cbe1ecf2b9e7af735b6
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-03-26 03:39:28 +0100

    Added get_mean_vol in analyzer class

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,9 @@
 class analyzer():
-    def __init__(self, data, num_of_candlesticks=30):
+    def __init__(self, df, num_of_candlesticks=30):
         self.num_of_candlesticks = num_of_candlesticks
-        self.data = data[:num_of_candlesticks]
+        self.df = df[:num_of_candlesticks]
+
+    def get_mean_vol(self):
+        return self.df['Volume'].mean()
+
 
