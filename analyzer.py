92693ad83737b0fed099b9942fd8f6cb42c7956c
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-04-13 21:45:35 +0200

    Added _create_candlesticks_list, created list of candlesticks object out for data from binance api

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,14 @@
 from candlestick import candlestick
+
+
 class analyzer():
     def __init__(self, candlestick_data, num_of_candlesticks=30):
         self.candlestick_data = candlestick_data
         self.num_of_candlesticks = num_of_candlesticks
-        self._candlesticks(candlestick_data)
+        self._create_candlesticks_list(candlestick_data[:num_of_candlesticks])
 
-    def _candlesticks(self, candlestick_data):
+    def _create_candlesticks_list(self, candlestick_data):
         candlesticks_data = candlestick_data[:self.num_of_candlesticks]
-        print(candlesticks_data)
+        # Ignores last value which is irrelevant
+        candlestick_list = [candlestick(*candle[:11]) for candle in candlesticks_data]
+        return candlestick_list
